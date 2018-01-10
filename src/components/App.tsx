b45b4d8f6c1663d8aeff7c1ba537873eb3024e61
DannyRyman <mail.dan.ryan@googlemail.com>
2018-01-10 16:01:41 +0000

    Convert add to stateless component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,15 @@ import * as React from 'react'
 import './App.css'
 import JsEditor from './JsEditor'
 
-class App extends React.Component {
-  render () {
-    return (
-      <div className='App'>
-        <div className='App-header'>
-          <h2>DynamoDB Query Analyzer</h2>
-        </div>
-        <div className='Editor'>
-          <JsEditor />
-        </div>
+export default function () {
+  return (
+    <div className='App'>
+      <div className='App-header'>
+        <h2>DynamoDB Query Analyzer</h2>
       </div>
-    )
-  }
+      <div className='Editor'>
+        <JsEditor />
+      </div>
+    </div>
+  )
 }
-
-export default App
