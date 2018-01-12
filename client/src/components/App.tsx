43de12004382f795dd6a0fc9926a417c1595ba5e
dan ryan <daniel.ryan@mergermarket.com>
2018-01-12 18:41:29 +0000

    Introduce API

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import JsEditor from './JsEditor'
 import { Button } from 'material-ui'
 import { withState, compose } from 'recompose'
-import { executeScript } from '../lib/execute-script'
+// import { executeScript } from '../lib/execute-script'
 import Header from './Header'
 import LogOutput from './LogOutput'
 
@@ -15,18 +15,21 @@ interface AppProps {
 }
 
 const App: React.SFC<AppProps> = ({ code, setCode, logs, setLogs }) => {
-  const log = (logEntry: string) => {
-    setLogs((existingEntries: string[]) => {
-      return [...existingEntries, logEntry]
-    })
-  }
+  // const log = (logEntry: string) => {
+  //   setLogs((existingEntries: string[]) => {
+  //     return [...existingEntries, logEntry]
+  //   })
+  // }
   const runClicked = () => {
-    console.log('run clicked!', code)
-    const context = {
-      log
-    }
-    const result = executeScript(code, context)
-    console.log('result', result)
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(json => console.log('response', json))
+    // console.log('run clicked!', code)
+    // const context = {
+    //   log
+    // }
+    // const result = executeScript(code, context)
+    // console.log('result', result)
   }
 
   return (
---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import * as express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
