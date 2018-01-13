d49b06cd42774d66d518a29a5c0c73294bee0e52
dan ryan <daniel.ryan@mergermarket.com>
2018-01-13 20:53:43 +0000

    Add table query endpoint

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,27 @@
 import * as express from 'express'
+import * as aws from 'aws-sdk'
 
 const app = express()
 
 const port = process.env.PORT || 3001
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' })
+const dynamoClient = createDynamoDocumentClient()
+
+function createDynamoDocumentClient () {
+  return new aws.DynamoDB.DocumentClient({
+    region: 'local',
+    endpoint: 'http://dynamodb:8000'
+  })
+}
+
+app.get('/api/table/:table', (req, res) => {
+  const table = req.params.table
+  const exclusiveStartKeyQueryString = req.query.exclusiveStartKey && (JSON.parse(decodeURIComponent(req.query.exclusiveStartKey)) as aws.DynamoDB.Key)
+  const exclusiveStartKey = exclusiveStartKeyQueryString
+  return dynamoClient.scan({
+    TableName: table,
+    ExclusiveStartKey: exclusiveStartKey
+  }).promise().then(results => res.send(results))
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
